f8d4bbcfe869aeea7db248b92f42af15e35160f3
Nirav Patel <14833460+niravinfo@users.noreply.github.com>
2026-06-15 09:10:44 +0530

    Add JobDistributionStrategyDemo project with GroupKey logic

---
+++ JobDistributionStrategyDemo/DistributionStrategies/GroupKeyConstants.cs
@@ -0,0 +1,9 @@
+namespace JobDistributionStrategyDemo.DistributionStrategies;
+
+public static class GroupKeyConstants
+{
+    public const string PropertyName = "GroupKey";
+    public const string JobTypePropertyKey = "GroupKeyProperty";
+    public const string MaxConcurrentGroupsKey = "MaxConcurrentGroups";
+    public const int DefaultMaxConcurrentGroups = 4;
+}
